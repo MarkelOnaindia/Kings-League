053f39ea464d8038138a381f4ee4175fb03c40ea
MarkelOnaindia <126677198+MarkelOnaindia@users.noreply.github.com>
2023-05-03 09:35:11 +0200

    añadir carpeta con las inserts
    de momento solo estan las inserts de propetario, staff, entrenador y equipo

---
+++ BBDD/inserts/equiposInsert.sql
@@ -0,0 +1,13 @@
+select * from equipo;
+INSERT INTO Equipo VALUES (DEFAULT, 'Porcinos FC', 'Rosa', 'Rosa', 150000000);
+INSERT INTO Equipo VALUES (DEFAULT, '1K', 'Azul', 'Rosa claro', 50000000);
+INSERT INTO Equipo VALUES (DEFAULT, 'Kunisports', 'Rosa y negro', 'Negro', 100000000);
+INSERT INTO Equipo VALUES (DEFAULT, 'Saiyans FC', 'Amarillo, azul', 'Azul claro', 80000000);
+INSERT INTO Equipo VALUES (DEFAULT, 'Jijantes FC', 'Rojo y azul', 'Rojo', 120000000);
+INSERT INTO Equipo VALUES (DEFAULT, 'Los Troncos FC', 'Verde y blanco', 'Verde y negro', 90000000);
+INSERT INTO Equipo VALUES (DEFAULT, 'Aniquiladores', 'Rojo y blanco', 'Rojo', 50000000);
+INSERT INTO Equipo VALUES (DEFAULT, 'Pio', 'Blanco', 'Blanco y negro', 70000000);
+INSERT INTO Equipo VALUES (DEFAULT, 'Rayo Barcelona', 'Blanco', 'Blanco', 180000000);
+INSERT INTO Equipo VALUES (DEFAULT, 'El Barrio', 'Azu', 'Azul', 60000000);
+INSERT INTO Equipo VALUES (DEFAULT, 'xBuyer Team', 'Negro', 'Naranja', 30000000);
+INSERT INTO Equipo VALUES (DEFAULT, 'Ultimate Móstoles', 'Verde', 'Negro', 45000000);
