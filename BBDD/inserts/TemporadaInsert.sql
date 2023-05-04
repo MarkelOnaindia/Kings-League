bda9493a809eb8f96067c86af1bef68589c93831
Ibaiber <127839087+Ibaiber@users.noreply.github.com>
2023-05-04 11:59:22 +0200

    Cambio Temporada

--- BBDD/inserts/TemporadaInsert.sql
+++ BBDD/inserts/TemporadaInsert.sql
@@ -1,4 +1,5 @@
-/*Temporada */
+/*Kings League equipo 2*/
+/*Insercion del Temporada*/
 
 INSERT INTO Temporada VALUES (DEFAULT, 'Invierno', 'Abierto');
 INSERT INTO Temporada VALUES (DEFAULT, 'Verano', 'Cerrado');
