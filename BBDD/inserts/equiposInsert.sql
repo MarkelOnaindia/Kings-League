5a6d061c6d5c4fda26985871fb4f137f6b622ed5
Ibaiber <127839087+Ibaiber@users.noreply.github.com>
2023-05-04 12:00:51 +0200

    Cambios Equipo

--- BBDD/inserts/equiposInsert.sql
+++ BBDD/inserts/equiposInsert.sql
@@ -1,4 +1,5 @@
-select * from equipo;
+/*Kings League equipo 2*/
+/*Insercion del Equipo*/
 INSERT INTO Equipo VALUES (DEFAULT, 'Porcinos FC', 'Rosa', 'Rosa', 150000000);
 INSERT INTO Equipo VALUES (DEFAULT, '1K', 'Azul', 'Rosa claro', 50000000);
 INSERT INTO Equipo VALUES (DEFAULT, 'Kunisports', 'Rosa y negro', 'Negro', 100000000);
@@ -10,4 +11,4 @@ INSERT INTO Equipo VALUES (DEFAULT, 'Pio', 'Blanco', 'Blanco y negro', 70000000)
 INSERT INTO Equipo VALUES (DEFAULT, 'Rayo Barcelona', 'Blanco', 'Blanco', 180000000);
 INSERT INTO Equipo VALUES (DEFAULT, 'El Barrio', 'Azu', 'Azul', 60000000);
 INSERT INTO Equipo VALUES (DEFAULT, 'xBuyer Team', 'Negro', 'Naranja', 30000000);
-INSERT INTO Equipo VALUES (DEFAULT, 'Ultimate Móstoles', 'Verde', 'Negro', 45000000);
+INSERT INTO Equipo VALUES (DEFAULT, 'Ultimate MÃ³stoles', 'Verde', 'Negro', 45000000);
