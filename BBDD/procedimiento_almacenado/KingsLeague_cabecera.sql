d7134869d33c368e2a41e09a17b908e6ad4c4d17
MarkelOnaindia <126677198+MarkelOnaindia@users.noreply.github.com>
2023-05-04 13:32:30 +0200

    Delete KingsLeague_cabecera.sql

--- BBDD/procedimiento_almacenado/KingsLeague_cabecera.sql
+++
@@ -1,4 +0,0 @@
-CREATE OR REPLACE PACKAGE relacion_equipo AS
-    PROCEDURE info_equipos(informacion_equ OUT SYS_REFCURSOR);
-    PROCEDURE info_jugadores(informacion_jug OUT SYS_REFCURSOR);
-END relacion_equipo;
