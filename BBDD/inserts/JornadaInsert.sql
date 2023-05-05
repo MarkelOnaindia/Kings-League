68af186df5ff308b4394d1aaf7e0a8ad69951f36
MarkelOnaindia <126677198+MarkelOnaindia@users.noreply.github.com>
2023-05-05 09:05:45 +0200

    Update JornadaInsert.sql
    añadido el dia

--- BBDD/inserts/JornadaInsert.sql
+++ BBDD/inserts/JornadaInsert.sql
@@ -1,15 +1,15 @@
 /*Kings League equipo 2*/
 /*Insercion del Jornada*/
-INSERT INTO Jornada VALUES (DEFAULT,1, 1, 'FaseRegular');
-INSERT INTO Jornada VALUES (DEFAULT,1, 2, 'FaseRegular');
-INSERT INTO Jornada VALUES (DEFAULT,1, 3, 'FaseRegular');
-INSERT INTO Jornada VALUES (DEFAULT,1, 4, 'FaseRegular');
-INSERT INTO Jornada VALUES (DEFAULT,1, 5, 'FaseRegular');
-INSERT INTO Jornada VALUES (DEFAULT,1, 6, 'FaseRegular');
-INSERT INTO Jornada VALUES (DEFAULT,1, 7, 'FaseRegular');
-INSERT INTO Jornada VALUES (DEFAULT,1,8, 'FaseRegular');
-INSERT INTO Jornada VALUES (DEFAULT,1, 9, 'FaseRegular');
-INSERT INTO Jornada VALUES (DEFAULT,1, 10, 'FaseRegular');
-INSERT INTO Jornada VALUES (DEFAULT,1, 11, 'FaseRegular');
-INSERT INTO Jornada VALUES (DEFAULT,1, 12, 'PlayOffs');
-INSERT INTO Jornada VALUES (DEFAULT,1, 13, 'PlayOffs');
+INSERT INTO Jornada VALUES (DEFAULT,1, 1,'01/01/2024', 'FaseRegular');
+INSERT INTO Jornada VALUES (DEFAULT,1, 2,'08/01/2024', 'FaseRegular');
+INSERT INTO Jornada VALUES (DEFAULT,1, 3,'15/01/2024', 'FaseRegular');
+INSERT INTO Jornada VALUES (DEFAULT,1, 4,'22/01/2024', 'FaseRegular');
+INSERT INTO Jornada VALUES (DEFAULT,1, 5,'29/01/2024', 'FaseRegular');
+INSERT INTO Jornada VALUES (DEFAULT,1, 6,'05/02/2024','FaseRegular');
+INSERT INTO Jornada VALUES (DEFAULT,1, 7,'12/02/2024', 'FaseRegular');
+INSERT INTO Jornada VALUES (DEFAULT,1,8,'19/02/2024', 'FaseRegular');
+INSERT INTO Jornada VALUES (DEFAULT,1, 9,'26/02/2024', 'FaseRegular');
+INSERT INTO Jornada VALUES (DEFAULT,1, 10,'05/03/2024', 'FaseRegular');
+INSERT INTO Jornada VALUES (DEFAULT,1, 11,'12/03/2024', 'FaseRegular');
+INSERT INTO Jornada VALUES (DEFAULT,1, 12,'19/03/2024', 'PlayOffs');
+INSERT INTO Jornada VALUES (DEFAULT,1, 13,'26/03/2024', 'PlayOffs');
