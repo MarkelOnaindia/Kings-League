0e10d2548119989c4c11f1270a17b9886ba5c272
MarkelOnaindia <126677198+MarkelOnaindia@users.noreply.github.com>
2023-05-08 12:19:41 +0200

    carpeta de vistas
    de momento la unica vista que tiene es la de la clasificacion

---
+++ BBDD/views/clasificacion.sql
@@ -0,0 +1,22 @@
+/*Kings League equipo 2*/
+/*View clasificacion*/
+CREATE OR REPLACE VIEW clasificacion AS
+SELECT
+    e.ID_EQUIPO,
+    e.NOMBRE,
+    COUNT(*) AS Numero_Victorias,
+    SUM(CASE WHEN p.GOLES_EQ1 > p.GOLES_EQ2 AND p.ID_GANADOR = e.ID_EQUIPO THEN p.GOLES_EQ1
+             WHEN p.GOLES_EQ2 > p.GOLES_EQ1 AND p.ID_GANADOR = e.ID_EQUIPO THEN p.GOLES_EQ2
+             ELSE 0 END) AS Goles_Anotados
+FROM
+    Equipo e
+    JOIN Partido p ON e.ID_EQUIPO = p.ID_GANADOR
+GROUP BY
+    e.ID_EQUIPO,
+    e.NOMBRE
+ORDER BY
+    Numero_Victorias DESC, 
+    Goles_Anotados DESC;
+
+select * from clasificacion;
+
