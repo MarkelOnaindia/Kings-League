9d924160d031719bd5048d0c04374424fde1a823
MarkelOnaindia <126677198+MarkelOnaindia@users.noreply.github.com>
2023-05-09 12:05:41 +0200

    Update MAX_JUGADORES.sql

--- BBDD/triggers/MAX_JUGADORES.sql
+++ BBDD/triggers/MAX_JUGADORES.sql
@@ -1,5 +1,5 @@
-DROP TRIGGER MAX_JUGADORES;
-
+/*Kings League equipo 2*/
+/*Trigger para controlar el maximo de los jugadores*/
 CREATE OR REPLACE TRIGGER MAX_JUGADORES
 FOR INSERT OR UPDATE ON ContratoJugador 
 COMPOUND TRIGGER
@@ -63,12 +63,12 @@ SELECT * FROM EQUIPO;
     INSERT INTO ContratoJugador VALUES (DEFAULT,10,1,10000000,TO_DATE('12/09/2022', 'DD/MM/YYYY'),TO_DATE('10/10/2022', 'DD/MM/YYYY'),1000000,12); --A la hora de insertar el noveno jugadro habitual salta el trigger
 
     /*Respuesta a la hora de la ultima insercion:
-    Error que empieza en la línea: 73 del comando :
+    Error que empieza en la lÃ­nea: 73 del comando :
     INSERT INTO ContratoJugador VALUES (DEFAULT,10,1,10000000,TO_DATE('12/09/2022', 'DD/MM/YYYY'),TO_DATE('10/10/2022', 'DD/MM/YYYY'),1000000,12)
     Informe de error -
     ORA-20000: NO SE PUEDEN TENER MAS DE 8 JUGADORES DE TIPO HABITUAL
-    ORA-06512: en "EQDAW02.MAX_JUGADORES", línea 28
-    ORA-04088: error durante la ejecución del disparador 'EQDAW02.MAX_JUGADORES'
+    ORA-06512: en "EQDAW02.MAX_JUGADORES", lÃ­nea 28
+    ORA-04088: error durante la ejecuciÃ³n del disparador 'EQDAW02.MAX_JUGADORES'
     */
 
     /*Comprobacion de wild cards*/
@@ -78,10 +78,10 @@ SELECT * FROM EQUIPO;
 
     
     /*Respuesta a la hora de la ultima insercion:
-    Error que empieza en la línea: 77 del comando :
+    Error que empieza en la lÃ­nea: 77 del comando :
     INSERT INTO ContratoJugador VALUES (DEFAULT,133,1,10000000,TO_DATE('12/09/2022', 'DD/MM/YYYY'),TO_DATE('10/10/2022', 'DD/MM/YYYY'),1000000,12)
     Informe de error -
     ORA-20001: NO SE PUEDEN TENER MAS DE 2 JUGADORES DE TIPO WILDCARD
-    ORA-06512: en "EQDAW02.MAX_JUGADORES", línea 33
-    ORA-04088: error durante la ejecución del disparador 'EQDAW02.MAX_JUGADORES'
+    ORA-06512: en "EQDAW02.MAX_JUGADORES", lÃ­nea 33
+    ORA-04088: error durante la ejecuciÃ³n del disparador 'EQDAW02.MAX_JUGADORES'
     */  
