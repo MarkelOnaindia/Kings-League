41d6161d3a4db042838592f70d9cae7a732ad146
MarkelOnaindia <126677198+MarkelOnaindia@users.noreply.github.com>
2023-05-12 09:49:30 +0200

    vuew ultimajornada
    view que muestra toda la informacion referente a la ultima jordana

---
+++ BBDD/views/ultimaJornada.sql
@@ -0,0 +1,25 @@
+/*Kings League equipo 2*/
+/*View para visualizar toda la informacion de la ultima jornada*/
+
+CREATE OR REPLACE VIEW vista_ultima_jornada AS
+SELECT
+    t.ID_TEMP,
+    t.TIPO AS Temporada,
+    j.ID_JOR,
+    j.NUMERO AS Numero_Jornada,
+    j.Dia,
+    j.TIPO AS Tipo_Jornada,
+    p.ID_PARTIDO,
+    p.HORA,
+    p.GOLES_EQ1,
+    p.GOLES_EQ2,
+    p.ID_Ganador,
+    p.ID_Perdedor
+FROM
+    Temporada t
+    INNER JOIN Jornada j ON t.ID_TEMP = j.ID_TEMP
+    INNER JOIN Partido p ON j.ID_JOR = p.ID_JOR
+WHERE
+    j.ID_JOR = (SELECT MAX(ID_JOR)
+                FROM Jornada);
+                
