30f4c971d253a3207c14c9c574f42919d4b20a2f
Ibaiber <127839087+Ibaiber@users.noreply.github.com>
2023-05-17 18:20:25 +0200

    Update comprobacionNoEpate.sql

--- BBDD/triggers/noEmpates/comprobacionNoEpate.sql
+++ BBDD/triggers/noEmpates/comprobacionNoEpate.sql
@@ -2,11 +2,11 @@
 /*Comprobacion trigger que evita los empates*/
 
 /*select * from partido;*/
-INSERT INTO Partido VALUES (DEFAULT,1, '16:00', 3, 3,5,6);
+INSERT INTO Partidos VALUES (DEFAULT,1, '16:00', 3, 3,5,6);
 
 /*Al intentar insertar un partido acabado en empate salta el error
-Error que empieza en la línea: 13 del comando :
-INSERT INTO Partido VALUES (DEFAULT,1, '16:00', 3, 3,5,6)
+Error que empieza en la lÃ­nea: 13 del comando :
+INSERT INTO Partidos VALUES (DEFAULT,1, '16:00', 3, 3,5,6)
 Informe de error -
 ORA-20001: No se permiten empates en los partidos.
 */
