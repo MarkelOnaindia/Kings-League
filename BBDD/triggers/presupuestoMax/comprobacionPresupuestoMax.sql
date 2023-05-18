8b342f2ff1c44f5dd525bc7deb755fc15c7ca947
MarkelOnaindia <126677198+MarkelOnaindia@users.noreply.github.com>
2023-05-18 09:01:39 +0200

    Update comprobacionPresupuestoMax.sql

--- BBDD/triggers/presupuestoMax/comprobacionPresupuestoMax.sql
+++ BBDD/triggers/presupuestoMax/comprobacionPresupuestoMax.sql
@@ -1,12 +1,12 @@
 /*Kings League equipo 2*/
 /*Comprobacion trigger maximo de presupuesto*/
 
-INSERT INTO ContratoJugadores VALUES (DEFAULT,4,3,10000000,TO_DATE('12/09/2022', 'DD/MM/YYYY'),TO_DATE('10/10/2022', 'DD/MM/YYYY'),200000000,1);
+INSERT INTO ContratosJugador VALUES (DEFAULT,4,3,10000000,TO_DATE('12/09/2022', 'DD/MM/YYYY'),TO_DATE('10/10/2022', 'DD/MM/YYYY'),200000000,1);
 /*Probamos a meter un jugador que supere los 200.0000.000€*/
 
 /* y nos salta el error
 
-INSERT INTO ContratoJugadores VALUES (DEFAULT,4,3,10000000,TO_DATE('12/09/2022', 'DD/MM/YYYY'),TO_DATE('10/10/2022', 'DD/MM/YYYY'),200000000,1)
+INSERT INTO ContratosJugador VALUES (DEFAULT,4,3,10000000,TO_DATE('12/09/2022', 'DD/MM/YYYY'),TO_DATE('10/10/2022', 'DD/MM/YYYY'),200000000,1)
 Informe de error -
 ORA-20001: La suma del sueldo y clausula de los jugadores supera los 200000000
 ORA-06512: en "EQDAW02.TRG_CONTRATOJUGADOR", línea 9
