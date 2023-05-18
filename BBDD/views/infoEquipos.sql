98e01642eda90db757b688dc529f9d149dea9617
MarkelOnaindia <126677198+MarkelOnaindia@users.noreply.github.com>
2023-05-18 09:04:56 +0200

    Update infoEquipos.sql

--- BBDD/views/infoEquipos.sql
+++ BBDD/views/infoEquipos.sql
@@ -1,8 +1,8 @@
 -- Vista equipos
 CREATE OR REPLACE VIEW view_equipos AS
 SELECT DISTINCT e.id_equipo, e.Nombre, e.Color_Escudo, e.Equipacion, e.Presupuesto,
-        COUNT(ContratoJugador.id_jug) AS cantidad_jugadores
+        COUNT(ContratosJugador.id_jug) AS cantidad_jugadores
 FROM Equipos e
-INNER JOIN ContratoJugadores ON e.id_equipo = ContratoJugadores.id_equipo
+INNER JOIN ContratosJugador ON e.id_equipo = ContratosJugador.id_equipo
 GROUP BY e.id_equipo, e.Nombre, e.Color_Escudo, e.Equipacion, e.Presupuesto
 Order by ID_EQUIPO;
